f9d86ae08b99b05d4e3ef3332ca43ce07b5e69bf
shlevanto <simo.levanto@helsinki.fi>
2021-11-24 22:24:29 +0200

    Module for database connection using sqlite3. The database should be in the same folder as this module.

---
+++ src/db.py
@@ -0,0 +1,12 @@
+import sqlite3
+from flask import g
+
+
+DATABASE = './lukuvinkit.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
