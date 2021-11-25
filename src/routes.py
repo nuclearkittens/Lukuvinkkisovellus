b910d5b33bf7a7e81402275f985d57cfd407027c
shlevanto <simo.levanto@helsinki.fi>
2021-11-25 21:50:28 +0200

    Added a /try_db route to demonstrate that database is working.

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,5 @@
 from app import app
+from db import db
 from flask import render_template
 from services.user_service import user_service
 from repositories.user_repository import user_repository
@@ -14,3 +15,17 @@ def render_home():
 @app.route("/ping")
 def ping():
     return "Pong"
+
+@app.route("/try_db")
+def try_db():
+    sql = "INSERT INTO users (username, password) VALUES ('Paavo', 'Pesusieni22')"
+    db.session.execute(sql)
+    sql = "SELECT username FROM users"
+    result = db.session.execute(sql)
+    sql = "DELETE FROM users WHERE username='Paavo'"
+    db.session.commit()
+    row = result.fetchone()
+    name = row[0]
+
+    return f"heippa {name}"
+    
