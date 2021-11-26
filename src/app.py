94f9bcac1263f311c39038041f93c85eb79b8f04
aejmmark <aejmelaeus.markus@gmail.com>
2021-11-26 14:50:18 +0200

    Add template reloading and remove tract modification warning

--- src/app.py
+++ src/app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from os import getenv
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = getenv("SECRET_KEY")
 
 import routes
