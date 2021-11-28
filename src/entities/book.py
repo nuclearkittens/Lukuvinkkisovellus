81918d1e6b6778583219d5135c30d28a0bb87e64
Jirko Leivo <jirko.leivo@gmail.com>
2021-11-28 16:22:39 +0200

    Commented book.py

--- src/entities/book.py
+++ src/entities/book.py
@@ -1,9 +1,10 @@
 class Book:
+    # tags and related_courses as String-lists
     def __init__(self, writer, title, description, isbn = None, tags = None, related_courses = None):
         self._writer = writer
         self._title = title
         self._description = description
         self._isbn = isbn
-        self._tags = [tags]
-        self._related_courses = [related_courses]
+        self._tags = tags
+        self._related_courses = related_courses
 
