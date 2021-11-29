d929477ec84ffedc253ff8e1ccfcd661db2604de
shlevanto <simo.levanto@helsinki.fi>
2021-11-29 21:38:43 +0200

    Test database environment set up. Added setup and simple test for adding user.

---
+++ src/start.sh
@@ -0,0 +1,3 @@
+gnome-terminal -- ~/pgsql/bin/start-pg.sh
+export FLASK_ENV=development
+flask run
--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -1,17 +1,22 @@
 import unittest
 from repositories.user_repository import UserRepository
-import app
+giimport app
 from db import db
 
 
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
         db.session.execute("DELETE FROM users")
+        self.user_repository = UserRepository(db)
 
-    def test_nothing(self):
+    def test_db_connection(self):
         db.session.execute(""" 
-            INSERT INTO users (username, password) 
-            VALUES ('Paavo', 'pesusieni')
+            INSERT INTO users (username, password)             VALUES ('Paavo', 'pesusieni')
 
             """)
         db.session.commit()
+    
+    def test_add_user(self):
+        result = self.user_repository.add_user('Paavo', 'pesusieni')
+        
+        self.assertTrue(result)
