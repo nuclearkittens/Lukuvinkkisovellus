deac00c09fca7fc1ac03ccb9cc682c2050ca73cd
Jirko Leivo <jirko.leivo@gmail.com>
2021-11-30 10:09:40 +0200

    Cleaned book.py

--- src/entities/book.py
+++ src/entities/book.py
@@ -1,10 +1,13 @@
 class Book:
     # tags and related_courses as String-lists
-    def __init__(self, writer, title, description, isbn = None, tags = None, related_courses = None):
-        self._writer = writer
+    def __init__(self, author, title, description, isbn = None, tags = None, related_courses = None):
+        self._author = author
         self._title = title
         self._description = description
         self._isbn = isbn
         self._tags = tags
         self._related_courses = related_courses
+
+    # TARKISTA PSQL ALUSTUS ASIA
+    # psql lukuvinkit < schema.sql
 
