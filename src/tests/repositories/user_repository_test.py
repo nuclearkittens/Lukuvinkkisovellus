d564c04348e87a1d2cacb3c2aaa517f49ed946b7
shlevanto <simo.levanto@helsinki.fi>
2021-11-30 21:39:34 +0200

    Fixed user repository testing. Import order is essential: import app before importing db from db.

--- src/tests/repositories/user_repository_test.py
+++ src/tests/repositories/user_repository_test.py
@@ -1,13 +1,15 @@
 import unittest
 from repositories.user_repository import UserRepository
-from db import db
 from entities.user import User
+import app
+from db import db
 
 
 class TestUserRepository(unittest.TestCase):
     def setUp(self):
         db.session.execute("DELETE FROM users")
         self.user_repository = UserRepository(db)
+        self.user = User(username='Paavo', password='Pesusieni')
 
     def test_db_connection(self):
         db.session.execute(""" 
@@ -15,8 +17,8 @@ class TestUserRepository(unittest.TestCase):
 
             """)
         db.session.commit()
-
+    
     def test_add_user(self):
-        result = self.user_repository.add_user(User('Paavo', 'pesusieni'))
-
+        result = self.user_repository.add_user(self.user)
+        
         self.assertTrue(result)
