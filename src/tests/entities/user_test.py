9de41aec5242b4b02300af5d3436d9f4f99d3103
shlevanto <simo.levanto@helsinki.fi>
2021-12-01 13:11:25 +0200

    test for proper construction of user class object

--- src/tests/entities/user_test.py
+++ src/tests/entities/user_test.py
@@ -6,5 +6,8 @@ class TestUser(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_nothing(self):
-        pass
+    def test_user_is_constructed_correctly(self):
+        user = User(username="Paavo", password="pesusieni")
+        
+        self.assertEqual("Paavo", user.username)
+        self.assertEqual("pesusieni", user.password)
