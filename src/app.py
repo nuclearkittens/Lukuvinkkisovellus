a59c2ceed8bca8156bc676f625275af79cd5dd73
aejmmark <aejmelaeus.markus@gmail.com>
2021-12-01 11:58:01 +0200

    Try possible fix to heroku crash

--- src/app.py
+++ src/app.py
@@ -3,8 +3,8 @@ from flask import Flask
 from config import SECRET_KEY
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["TEMPLATES_AUTO_RELOAD"] = True
+#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#app.config["TEMPLATES_AUTO_RELOAD"] = True
 app.secret_key = SECRET_KEY
 
 import routes
