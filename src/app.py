63d2ba868055293365227b635bd58a3223e5168c
aejmmark <aejmelaeus.markus@gmail.com>
2021-12-01 12:33:35 +0200

    Experimental heroku config fix

--- src/app.py
+++ src/app.py
@@ -1,10 +1,13 @@
 
 from flask import Flask
-from config import SECRET_KEY
+from os import getenv
+#from config import SECRET_KEY
 
 app = Flask(__name__)
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-#app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.secret_key = SECRET_KEY
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+#app.secret_key = SECRET_KEY
 
 import routes
