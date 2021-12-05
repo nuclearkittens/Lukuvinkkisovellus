a99022a5609d324ba27074281e2560fda2e2297b
shlevanto <simo.levanto@helsinki.fi>
2021-12-05 18:25:17 +0200

    appease pylint

--- src/tests/services/blog_service_test.py
+++ src/tests/services/blog_service_test.py
@@ -11,12 +11,12 @@ class StubRepo:
     def add_blog(self, blog, user_id):
         self.blogs.append((user_id, blog))
         self.blog_id += 1
-        
+
         return True
 
     def get_users_blogs(self, user_id):
-        return [blog for blog in self.blogs if blog[0] == user_id]    
-    
+        return [blog for blog in self.blogs if blog[0] == user_id]
+
 
 class TestblogService(unittest.TestCase):
     def setUp(self):
@@ -26,23 +26,23 @@ class TestblogService(unittest.TestCase):
         self.user_id = 1
         self.other_user_id = 2
         self.service.new_blog(self.test_blog, self.user_id)
-      
+
     def test_user_can_add_blog(self):
         a_blog = Blog("author2", "title2", "url2", "a fine blog")
         response = self.service.new_blog(a_blog, self.user_id)
-        
+
         self.assertTrue(response)
-    
+
     def test_user_can_get_list_of_their_blogs(self):
         blogs = self.service.get_my_blogs(self.user_id)
-    
+
         self.assertEqual(len(blogs), 1)
         self.assertEqual(blogs[0][0], self.user_id)
-    
+
     def test_user_only_gets_their_own_blogs(self):
         a_blog = Blog("another author2", "another tilte2", "some url", "a good read")
         self.service.new_blog(a_blog, self.other_user_id)
         blogs = self.service.get_my_blogs(self.user_id)
-    
+
         self.assertEqual(len(blogs), 1)
         self.assertEqual(blogs[0][0], self.user_id)
--- src/tests/services/book_service_test.py
+++ src/tests/services/book_service_test.py
@@ -11,12 +11,12 @@ class StubRepo:
     def add_book(self, book, user_id):
         self.books.append((user_id, book))
         self.book_id += 1
-        
+
         return True
 
     def get_users_books(self, user_id):
         return [book for book in self.books if book[0] == user_id]    
-    
+
 
 class TestBookService(unittest.TestCase):
     def setUp(self):
@@ -26,23 +26,23 @@ class TestBookService(unittest.TestCase):
         self.user_id = 1
         self.other_user_id = 2
         self.service.new_book(self.test_book, self.user_id)
-      
+
     def test_user_can_add_book(self):
         a_book = Book("another author", "another tilte", "isbn+1", "a good read")
         response = self.service.new_book(a_book, self.user_id)
-        
+
         self.assertTrue(response)
-    
+
     def test_user_can_get_list_of_their_books(self):
         books = self.service.get_my_books(self.user_id)
-    
+
         self.assertEqual(len(books), 1)
         self.assertEqual(books[0][0], self.user_id)
-    
+
     def test_user_only_gets_their_own_books(self):
         a_book = Book("another author", "another tilte", "isbn+1", "a good read")
         self.service.new_book(a_book, self.other_user_id)
         books = self.service.get_my_books(self.user_id)
-    
+
         self.assertEqual(len(books), 1)
         self.assertEqual(books[0][0], self.user_id)
