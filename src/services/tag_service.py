6d07b938ca1257758aff12ecfbf35df8a9c015e7
Topias Harjunpää <topias.harjunpaa@gmail.com>
2021-12-11 19:47:10 +0200

    Remove self from repository methods params

--- src/services/tag_service.py
+++ src/services/tag_service.py
@@ -3,10 +3,10 @@ class TagService:
         self._tag_repository = tag_repository
     
     def get_tags(self, user_id):
-        return self._tag_repository.get_tags(self, user_id)
+        return self._tag_repository.get_tags(user_id)
 
     def add_tag(self, tag, user_id):
-        return self._tag_repository.add_tag(self,tag, user_id)
+        return self._tag_repository.add_tag(tag, user_id)
 
     def edit_tag(self, tag_id, edited_tag):
         return self._tag_repository.edit_tag(tag_id, edited_tag)
